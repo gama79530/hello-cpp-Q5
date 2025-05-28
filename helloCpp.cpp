6748529b08f8e7d79fd031d28e9eb1ba2e383fb3
Rein Wu <Rein.Wu@axiomtek.com.tw>
2025-05-28 15:32:23 +0800

    Add a for loop code for debug tool demo

--- helloCpp.cpp
+++ helloCpp.cpp
@@ -2,4 +2,9 @@
 
 int main(void) {
   std::cout << "Hello world message for Quest 5 by C++ !!!" << std::endl;
+  for (int i = 0; i < 5; i++) {
+    std::cout << "For Debug Demo, i = " << i << std::endl;
+  }
+
+  return 0;
 }
